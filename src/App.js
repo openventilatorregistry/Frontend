c1815a0889c25cd22503582d5ce4c54a25cbdb51
Colin Mccarthy <chmccc@gmail.com>
2020-03-29 14:22:59 -0700

    refactor: change top level App to functional

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import React, { Component } from 'react'
-import imageHero from './images/hero.png'
+import React from "react";
+import imageHero from "./images/hero.png";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  /**
-   * Component Did Mount
-   */
-
-  async componentDidMount() {}
-
-  /**
-   * Render
-   */
-
-  render() {
-    return (
-      <div className="container">
-        <div className="hero">
-          <img src={imageHero} />
-        </div>
-
-        <div className="tagline">
-          a website built on serverless components via the serverless framework
-        </div>
+export const App = () => {
+  return (
+    <div className="container">
+      <div className="hero">
+        <img src={imageHero} />
       </div>
-    )
-  }
-}
+
+      <div className="tagline">
+        a website built on serverless components via the serverless framework
+      </div>
+    </div>
+  );
+};
